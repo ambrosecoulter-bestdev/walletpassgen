35754e6b2d15b8ee42fa37a8d4461a401b8abd58
ambrosecoulter <84236650+ambrosecoulter@users.noreply.github.com>
2022-06-09 15:54:35 +1000

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -83,7 +83,8 @@ def test_gen():
     passfile.backgroundColor = request.form['BackgroundColor']
     passfile.foregroundColor = request.form['ForegroundColor']
     passfile.labelColor = request.form['LabelColor']
-    passfile.ibeacons = IBeacon("A648F270-BC33-483B-8614-E30A26B55FAA", "0", "0")
+    beacondata = IBeacon("A648F270-BC33-483B-8614-E30A26B55FAA", "0", "0")
+    passfile.ibeacons = beacondata
 
 
 
