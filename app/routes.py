07b3104b3e33381effa7de5178f665a5a206eeb2
ambrosecoulter <84236650+ambrosecoulter@users.noreply.github.com>
2022-06-09 16:01:13 +1000

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -83,7 +83,7 @@ def test_gen():
     passfile.backgroundColor = request.form['BackgroundColor']
     passfile.foregroundColor = request.form['ForegroundColor']
     passfile.labelColor = request.form['LabelColor']
-    passfile.ibeacons = [IBeacon("A648F270-BC33-483B-8614-E30A26B55FAA", "0", "0"), ]
+    passfile.ibeacons = [IBeacon("efb8454c-6988-11eb-9439-0242ac130002", "0", "0"), ]
 
 
 
