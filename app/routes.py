5e3607c3bc5238fa412422dbe1d80d0fd547d150
ambrosecoulter <84236650+ambrosecoulter@users.noreply.github.com>
2022-06-09 15:23:56 +1000

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -13,7 +13,7 @@ from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 
-from wallet.models import Pass, Barcode, StoreCard, EventTicket
+from wallet.models import Pass, Barcode, StoreCard, EventTicket, Generic
 
 import json
 
@@ -52,51 +52,54 @@ def test_gen():
     ###
     if request.form['TicketType'] == 'EventTicket':
         cardInfo = EventTicket()
-        
-        passfile = Pass(cardInfo, organizationName='Org Name', passTypeIdentifier='pass.com.spectrum.ticketpass', teamIdentifier='PFWC6XGUU8')
-        passfile.serialNumber = pkpassuuid
-
-
-
-        #FIELDS
-        #PRIMARY
-        if request.form['PrimaryField'] != '':
-            pfi = json.loads(request.form['PrimaryField'])
-            cardInfo.addPrimaryField(pfi["key"], pfi["value"], pfi["label"])
-        #SECONDARY
-        if request.form['SecondaryField'] != '':
-            sfi = json.loads(request.form['SecondaryField'])
-            cardInfo.addPrimaryField(sfi["key"], sfi["value"], sfi["label"])
-        #BACKFIELD
-        if request.form['PassBackInformation'] != '':
-            bffi = json.loads(request.form['PassBackInformation'])
-            cardInfo.addBackField(bffi["key"], bffi["value"], bffi["label"])
-        cardInfo.addBackField('disclaimer', 'This pass has been created "" for evaluation purposes using the third party service Spectrum which is owned and operated by Spectrum in Queensland Australia. The pass issuer has attested that any trademarks and/or copyrighted content contained within this pass are being used with the consent of the owner. Questions, complaints or claims with respect to this pass should be addressed to the pass issuer"". NEITHER APPLE NOR SPECTRUM SHALL be liable for any damages or losses arising for any use, distribution, misuse, reliance on, inability to use, interruption, suspension or termination of passbook, this pass, or any services provided in connection therewithin, including but not limited to any loss or failure to display your pass in passbook or any claim arising from any use of the foregoing by you, the pass issuer.', 'Disclaimer')
-
-
-        #CUSTOMISATION
-        passfile.description = request.form['Description']
-        passfile.backgroundColor = request.form['BackgroundColor']
-        passfile.foregroundColor = request.form['ForegroundColor']
-        passfile.labelColor = request.form['LabelColor']
-
-
-
-        #BARCODE
-        if request.form['Barcode'] != '':
-            bfi = json.loads(request.form['Barcode'])
-            barcodeFormat = bfi["barcodeFormat"]
-            stdBarcode = Barcode(bfi["value"], barcodeFormat, bfi["alternateText"])
-            passfile.barcode = stdBarcode
-    
-
-        
-        #LOGOS
-        passfile.addFile('icon.png', open('/root/walletpassgen/ticketimages/'+request.form['iconPath'], 'rb'))
-        passfile.addFile('icon@2x.png', open('/root/walletpassgen/ticketimages/'+request.form['iconPath'], 'rb'))
-        passfile.addFile('logo.png', open('/root/walletpassgen/ticketimages/'+request.form['logoPath'], 'rb'))
+    if request.form['TicketType'] == 'Generic':
+        cardInfo = Generic()
     else:
         return 'error'
+        
+    passfile = Pass(cardInfo, organizationName='Org Name', passTypeIdentifier='pass.com.spectrum.ticketpass', teamIdentifier='PFWC6XGUU8')
+    passfile.serialNumber = pkpassuuid
+
+
+
+    #FIELDS
+    #PRIMARY
+    if request.form['PrimaryField'] != '':
+        pfi = json.loads(request.form['PrimaryField'])
+        cardInfo.addPrimaryField(pfi["key"], pfi["value"], pfi["label"])
+    #SECONDARY
+    if request.form['SecondaryField'] != '':
+        sfi = json.loads(request.form['SecondaryField'])
+        cardInfo.addPrimaryField(sfi["key"], sfi["value"], sfi["label"])
+    #BACKFIELD
+    if request.form['PassBackInformation'] != '':
+        bffi = json.loads(request.form['PassBackInformation'])
+        cardInfo.addBackField(bffi["key"], bffi["value"], bffi["label"])
+    cardInfo.addBackField('disclaimer', 'This pass has been created "" for evaluation purposes using the third party service Spectrum which is owned and operated by Spectrum in Queensland Australia. The pass issuer has attested that any trademarks and/or copyrighted content contained within this pass are being used with the consent of the owner. Questions, complaints or claims with respect to this pass should be addressed to the pass issuer"". NEITHER APPLE NOR SPECTRUM SHALL be liable for any damages or losses arising for any use, distribution, misuse, reliance on, inability to use, interruption, suspension or termination of passbook, this pass, or any services provided in connection therewithin, including but not limited to any loss or failure to display your pass in passbook or any claim arising from any use of the foregoing by you, the pass issuer.', 'Disclaimer')
+
+
+    #CUSTOMISATION
+    passfile.description = request.form['Description']
+    passfile.backgroundColor = request.form['BackgroundColor']
+    passfile.foregroundColor = request.form['ForegroundColor']
+    passfile.labelColor = request.form['LabelColor']
+
+
+
+    #BARCODE
+    if request.form['Barcode'] != '':
+        bfi = json.loads(request.form['Barcode'])
+        barcodeFormat = bfi["barcodeFormat"]
+        stdBarcode = Barcode(bfi["value"], barcodeFormat, bfi["alternateText"])
+        passfile.barcode = stdBarcode
+
+
+    
+    #LOGOS
+    passfile.addFile('icon.png', open('/root/walletpassgen/ticketimages/'+request.form['iconPath'], 'rb'))
+    passfile.addFile('icon@2x.png', open('/root/walletpassgen/ticketimages/'+request.form['iconPath'], 'rb'))
+    passfile.addFile('logo.png', open('/root/walletpassgen/ticketimages/'+request.form['logoPath'], 'rb'))
+
      
 
 
